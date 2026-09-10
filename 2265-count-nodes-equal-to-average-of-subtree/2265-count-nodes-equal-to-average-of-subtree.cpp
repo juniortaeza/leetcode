a27fd578f00867be4054f251f2c5010c5b678d59
Junior Taeza <taeza@usc.edu>
2026-09-10 11:28:30 -0700

    Time: 10 ms (39.19%), Space: 15.6 MB (97.32%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -12,59 +12,25 @@
 class Solution {
 public:
 
-    void traverseSubtree(TreeNode* root, int& count, int& sum){
+    pair<int, int> dfs(TreeNode* root, int& valid){
         if(!root)
-            return ;
-        
-        count += 1;
-        sum += root->val;
+            return {0, 0};
 
-        traverseSubtree(root->left, count, sum);
-        traverseSubtree(root->right, count, sum);
+        pair<int, int> left = dfs(root->left, valid);
+        pair<int, int> right = dfs(root->right, valid);
+
+        int sum = root->val + left.first + right.first;
+        int count = 1 + left.second + right.second;
+
+        if(sum / count == root->val)
+            valid++;
+
+        return {sum, count};
     }
 
     int averageOfSubtree(TreeNode* root) {
-        
-        queue<TreeNode*> q;
-        q.push(root);
-
         int valid = 0;
-        while(!q.empty()){
-            TreeNode* node = q.front();
-            q.pop();
-
-            cout << node->val << endl;
-
-            int sum = 0, count = 0;
-            traverseSubtree(node, count, sum);
-
-            if(sum / count == node->val) valid += 1;
-        
-            if(node->left)  q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
-
-
+        dfs(root, valid);
         return valid;
     }
 };
-
-/*
-
-brute-force: for every node, traverse it's subtree counting nodes and adding vals -> compute average
-time:        O(n^2), traversing every node's subtree, which can get large
-space:       O(1), only need to store how many nodes in that subtree and the sum of it's nodes
-
-Input: root = [4,8,5,0,1,null,6]
-Output: 5
-Explanation: 
-For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
-For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
-For the node with value 0: The average of its subtree is 0 / 1 = 0.
-For the node with value 1: The average of its subtree is 1 / 1 = 1.
-For the node with value 6: The average of its subtree is 6 / 1 = 6.
-
-* return the number of nodes where the value of the node is equal 
-to the average of the values in its subtree
-
-*/
