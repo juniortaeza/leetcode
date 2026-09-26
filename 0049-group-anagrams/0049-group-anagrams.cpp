cce47899b0800167cc52d3b9e758ef5a54d8950f
Junior Taeza <taeza@usc.edu>
2026-09-26 10:10:36 -0700

    Time: 7 ms (98.77%), Space: 25.1 MB (64.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    /*
+        Time:               O(n*klogk + n), but klogk bounded k <= 100 thus constant = O(n + n) = O(2n) = O(n)
+        Auxiliary Space:    O(n), worst-case the strMap can grow to size strs if all strings are unique
+        Output Space:       O(n), result will store all strings in strs, just grouped anagramically
+    */
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> strMap;
         for(string str : strs){
